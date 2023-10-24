b18de1adc0283d05f420e48cc15bb1f9f797d421
Dusan Peric <dushanperic1@gmail.com>
2023-10-24 02:34:15 +0200

    Add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -20,12 +21,28 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body
-        className={`w-full h-full xl:h-screen flex items-start justify-start bg-[#faf9f7] ${inter.className}`}
-      >
-        <Main>{children}</Main>
-      </body>
-    </html>
+    <>
+      <html lang="en">
+        <head>
+          <Script
+            strategy="afterInteractive"
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-RK8EKF3QYT"
+          />
+          <Script id="google-analytics" async strategy="afterInteractive">
+            {`window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-RK8EKF3QYT');
+        `}
+          </Script>
+        </head>
+        <body
+          className={`w-full h-full xl:h-screen flex items-start justify-start bg-[#faf9f7] ${inter.className}`}
+        >
+          <Main>{children}</Main>
+        </body>
+      </html>
+    </>
   );
 }
